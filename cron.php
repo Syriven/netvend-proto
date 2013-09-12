8313866961a6a1d8b7590c9052ca8f57f95e17a9
Bardi Harborow <bardi_harborow@yahoo.com.au>
2013-09-12 09:13:41 -0400

    Comments

--- cron.php
+++ cron.php
@@ -1,4 +1,16 @@
 <?php
+/*!
+ * cron.php by @Syriven. Small help by @BardiHarborow. Part of the SocialNet\NetVend Project.
+ *
+ * Licensed under the CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+ * Get a copy at https://creativecommons.org/publicdomain/zero/1.0/
+ *
+ * Want to donate?
+ * NetVend is a concept first described by minisat_maker on Reddit.
+ * Syriven (1MphZghyHmmrzJUk316iHvZ55UthfHXR34, @Syriven) designed the first functional implementation of NetVend.
+ * Bardi Harborow (1Bardi4eoUvJomBEtVoxPcP8VK26E3Ayxn, @BardiHarborow) wrote the first apis for NetVend and converted the sever to JSONRPC.
+ */
+
 include("common.php");
 
 function get_address_info($address, $debug = false) {
