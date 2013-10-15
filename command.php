d407916b83827affe26025b80064a76e99cd8780
Logan Brutsche <syriven@gmail.com>
2013-10-15 15:20:15 -0800

    changed CORS settings

--- command.php
+++ command.php
@@ -340,7 +340,7 @@ class CommandHandler {
     */
     public function handleHTTPRequest() {
         if (isset($_SERVER['HTTP_ORIGIN'])) {
-            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header("Access-Control-Allow-Origin: *");
             header('Access-Control-Allow-Credentials: true');
             header('Access-Control-Max-Age: 86400'); // Cache for 1 day
         }
