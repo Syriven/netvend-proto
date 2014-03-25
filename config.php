80ace5d376b483b3bed1207227e4d13a30112bce
Logan Brutsche <syriven@gmail.com>
2014-03-25 09:43:22 -0800

    rewored the fees a bit

--- config.php
+++ config.php
@@ -6,12 +6,12 @@ $deposit_addr = "1GiHC8zWqst5dZCkYpwStEDfyyr8kFPWyU";
 $mysqli_link = new mysqli("localhost", DATABASE_INSERT_USERNAME, DATABASE_INSERT_PASS, DATABASE_NAME);
 
 /* Change these to your liking. */
-define("FEE_TIP", satoshis_to_usats(0.03));
+define("FEE_TIP", satoshis_to_usats(0.005));
 define("FEE_POST_BASE", satoshis_to_usats(0.003));
 define("FEE_POST_SIZE", satoshis_to_usats(0.0001));
 define("FEE_QUERY_BASE", satoshis_to_usats(0.001));
 define("FEE_QUERY_TIME", satoshis_to_usats(0.01));
-define("FEE_QUERY_SIZE", satoshis_to_usats(0.00001));
+define("FEE_QUERY_SIZE", satoshis_to_usats(0.000001));
 define("FEE_WITHDRAW", satoshis_to_usats(0.03));
 define("DEPOSIT_MIN_CONF", 0);
 define("FEE_TX", btc_to_usats(0.0005));
