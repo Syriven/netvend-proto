9e0ef2d9f3ed54b1e71f80b55ad7ad03a2d71db0
Syriven <syriven@gmail.com>
2014-03-26 23:34:30 -0700

    added in secret_values.php with empty strings

---
+++ secret_values.php
@@ -0,0 +1,11 @@
+<?php
+
+define("DATABASE_INSERT_USERNAME", "");
+define("DATABASE_INSERT_PASS", "");
+
+define("DATABASE_SELECT_USERNAME", "");
+define("DATABASE_SELECT_PASS", "");
+
+define("DATABASE_NAME", "netvend");
+
+?>
